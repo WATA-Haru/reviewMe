a7cd1cd24e18cf06a8dca9c54cfacfb0cc5b6678
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2024-11-05 14:42:55 +0900

    [add] just create Modal

--- A02_modal/vue-project/src/App.vue
+++ A02_modal/vue-project/src/App.vue
@@ -1,53 +1,15 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Modal from './components/Modal.vue'
 </script>
 
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <h1>This is a sample page.</h1>
   </header>
 
   <main>
-    <TheWelcome />
+    <Modal />
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
---
+++ A02_modal/vue-project/src/components/Modal.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const isShowModal = ref(false)
+</script>
+<template>
+  <button @click="isShowModal = true">showModal</button>
+  <Teleport to="body">
+    <div v-if="isShowModal">
+      <div class="modal-wrapper">
+        <div class="modal-mask">
+          <div class="modal">
+            <h2>This is a modal open</h2>
+            <button @click="isShowModal = false">close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Teleport>
+</template>
+<style lang="css" scoped>
+.modal-wrapper {
+  --modal-bg-color: rgba(0, 0, 0, 0.5);
+  --zindex-highest: 9999;
+}
+.modal-mask {
+  /* 公式docsから */
+  position: fixed;
+  z-index: var(--zindex-highest);
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: var(--modal-bg-color);
+  display: flex;
+  /* transition: opacity 0.3s ease; */
+}
+</style>
