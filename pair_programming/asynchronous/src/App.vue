a4b26d7ce9928a20a2b0f2071fdd1de60b1fddef
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2024-11-10 19:40:47 +0900

    [add] set default image and API type guard function

--- pair_programming/asynchronous/src/App.vue
+++ pair_programming/asynchronous/src/App.vue
@@ -1,24 +1,31 @@
 <script setup lang="ts">
-import { reactive, onMounted } from 'vue'
-const startPokeNum = 477
-interface Pokemon {
-  id: number
-  name: string
-  weight: number
-  sprites: {
-    front_default: string
-    front_shiny: string
-  }
+import axios from 'axios';
+import { ref, shallowRef, onMounted, type ShallowRef } from 'vue'
+
+type Pokemon = {
+  id: number;
+  name: string;
+  weight: number;
+  sprites: Sprites; 
 }
-const pokeData: Pokemon = reactive({
-  id: startPokeNum,
-  name: '',
+
+interface Sprites {
+  front_default:string,
+  front_shiny: string
+}
+
+const startPokeId = 477;
+const notImagePath = '/src/assets/No_Image_Available.jpg';
+const canNotGetImagePath = '/src/assets/canNotGetImagePath.jpg';
+const shallowPokeDataRef: ShallowRef<Pokemon> = shallowRef({
+  id: startPokeId,
+  name: 'Nan',
   weight: 0,
   sprites: {
-    front_default: '',
-    front_shiny: '',
-  },
-})
+    front_default: notImagePath,
+    front_shiny: notImagePath,
+  }
+});
 
 const getPokeData = async function (pokeNum: number): Promise<object | null> {
   const baseURL: string = 'https://pokeapi.co/api/v2/pokemon/'
@@ -38,37 +45,47 @@ const getPokeData = async function (pokeNum: number): Promise<object | null> {
   }
 }
 
-onMounted(() => {
-  setNextPokemon()
-})
-
-const setNextPokemon = async (): Promise<void> => {
-  pokeData.id++
-  const response = await getPokeData(pokeData.id)
-  if (!response) {
-    return null
+const isPokeTypeValid = (value: unknown | Pokemon): value is Pokemon => {
+  if (typeof value !== "object") {
+    return false;
   }
+  // https://typescriptbook.jp/reference/statements/unknown
+  const mayBePokemon = value as Record<keyof Pokemon, unknown>
   if (
-    'name' in response &&
-    'weight' in response &&
-    'sprites' in response
-  )
-  {
-    const {
-      name,
-      weight,
-      sprites
-    }  = response as {
-        name: string;
-        weight: number;
-        sprites: { front_default: string, front_shiny: string }
-    }
-    Object.assign(pokeData, {
-      name,
-      weight,
-      sprites,
-    })
+    typeof mayBePokemon.id === undefined &&
+    typeof mayBePokemon.name === undefined &&
+    typeof mayBePokemon.weight === undefined &&
+    mayBePokemon.sprites &&
+    typeof (mayBePokemon.sprites as Sprites).front_default === undefined &&
+    typeof (mayBePokemon.sprites as Sprites).front_shiny === undefined
+  ) {
+    return false;
   }
+  return true;
+}
+
+const isValid = (response: unknown): response is Pokemon => {
+  if (!response) {
+    return false;
+  }
+  if (!isPokeTypeValid) {
+    return false;
+  }
+  return true;
+}
+
+const setPokemon = async (): Promise<void> => {
+  const currentId = shallowPokeDataRef.value.id;
+  const response = await getPokeData(currentId + 1)
+
+  // if id is invalid set canNotGetImagePath to imagePath
+  if (!isValid(response)) {
+    shallowPokeDataRef.value.sprites.front_default = canNotGetImagePath;
+    shallowPokeDataRef.value.sprites.front_shiny = canNotGetImagePath;
+    return
+  }
+  //set pokeId
+  shallowPokeDataRef.value = response;
 }
 </script>
 
@@ -76,10 +93,11 @@ const setNextPokemon = async (): Promise<void> => {
   <h1>Pokemon</h1>
   <!-- <button @click="incrementNum">Next</button> -->
   <div class="wrapper">
-    <button @click="setNextPokemon()">next</button>
-    <h3>{{ pokeData.name }}</h3>
-    <img :src="pokeData.sprites.front_default" width="300px" height="300px" />
+    <button @click="setPokemon()">next</button>
+    <h3>{{ shallowPokeDataRef.name }}</h3>
+    <img :src="shallowPokeDataRef.sprites.front_default" width="300px" height="300px" />
   </div>
 </template>
 
 <style scoped></style>
+
