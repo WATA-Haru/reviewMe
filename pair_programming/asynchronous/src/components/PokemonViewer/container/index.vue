4a64fee5de6793f0456e9ee6bcde97e944df0936
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2024-11-14 10:48:05 +0900

    [refactor] devide ref to composables function

--- pair_programming/asynchronous/src/components/PokemonViewer/container/index.vue
+++ pair_programming/asynchronous/src/components/PokemonViewer/container/index.vue
@@ -1,61 +1,20 @@
 <script setup lang="ts">
-import { onMounted, shallowRef, type ShallowRef, triggerRef, watch } from 'vue'
-import { updatePokeData } from './composables/updatePokeData'
+import { useUpdatePokeData } from './composables/useUpdatePokeData'
 import PokemonViewerPresentation from '@/components/PokemonViewer/presentation/index.vue'
 
 defineOptions({
   name: 'PokemonViewerContainer',
 })
 
-const startPokeId = 9
-const notImagePath = '/src/assets/No_Image_Available.jpg'
-const shallowPokeDataRef: ShallowRef<Pokemon> = shallowRef({
-  id: startPokeId,
-  name: 'Nan',
-  weight: 0,
-  sprites: {
-    front_default: notImagePath,
-    front_shiny: notImagePath,
-  },
-})
-
-watch(
-  () => shallowPokeDataRef.value.id,
-  () => {
-    updatePokeData(shallowPokeDataRef)
-  },
-)
-onMounted(() => {
-  updatePokeData(shallowPokeDataRef)
-})
-
-const incrementId = () => {
-  shallowPokeDataRef.value.id++
-  triggerRef(shallowPokeDataRef)
-}
-
-const decrementId = () => {
-  shallowPokeDataRef.value.id--
-  triggerRef(shallowPokeDataRef)
-}
-
-document.addEventListener('keydown', (e: KeyboardEvent) => {
-  if (!e) {
-    return
-  } else if (e.key === 'ArrowLeft') {
-    decrementId()
-  } else if (e.key === 'ArrowRight') {
-    incrementId()
-  }
-})
+const { pokeDataRef, decrementRefID, incrementRefID } = useUpdatePokeData()
 </script>
 
 <template>
   <main class="main">
     <PokemonViewerPresentation
-      :poke-data="shallowPokeDataRef"
-      @prev-pokemon="decrementId"
-      @next-pokemon="incrementId"
+      :poke-data="pokeDataRef"
+      @prev-pokemon="decrementRefID"
+      @next-pokemon="incrementRefID"
     />
   </main>
 </template>
---
+++ pair_programming/asynchronous/src/const/constValues.ts
@@ -0,0 +1,2 @@
+export const START_POKE_ID: number = 266
+export const POKEAPI_URL: string = 'https://pokeapi.co/api/v2/pokemon/'
