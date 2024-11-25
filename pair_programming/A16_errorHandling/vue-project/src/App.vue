021dc9228dd5b3f80661c44728d164d59c51f33b
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2024-11-25 11:34:38 +0900

    [add] env setup

---
+++ pair_programming/A16_errorHandling/vue-project/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import ErrorHandling from './components/ErrorHandling.vue'
+</script>
+
+<template>
+  <main>
+    <ErrorHandling />
+  </main>
+</template>
+
+<style scoped></style>
