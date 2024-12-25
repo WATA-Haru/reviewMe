7acad594775e8974af6e4f58916bbd372057f585
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2024-12-25 23:21:44 +0900

    [ADD] isPostalCode test

---
+++ A15/src/utils/isPostalCode/index.test.ts
@@ -0,0 +1,26 @@
+import { describe, test, expect } from 'vitest'
+import { isPostalCode } from './index.ts'
+
+describe('isPostalCode test', () => {
+  test('郵便番号が000-000ときisPostalCodeがtrueを返すこと', () => {
+    expect(isPostalCode('000-0000')).toBe(true)
+  })
+  test('郵便番号が999-999ときisPostalCodeがtrueを返すこと', () => {
+    expect(isPostalCode('999-9999')).toBe(true)
+  })
+  test('郵便番号の上が3桁より大きい1111-000のときisPostalCodeがfalseを返すこと', () => {
+    expect(isPostalCode('0000-0000')).toBe(false)
+  })
+  test('郵便番号の上が3桁未満11-000のときisPostalCodeがfalseを返すこと', () => {
+    expect(isPostalCode('11-000')).toBe(false)
+  })
+  test('郵便番号の下が4桁より大きい111-00000のときisPostalCodeがfalseを返すこと', () => {
+    expect(isPostalCode('111-00000')).toBe(false)
+  })
+  test('郵便番号の下が4桁未満111-000のときisPostalCodeがfalseを返すこと', () => {
+    expect(isPostalCode('111-000')).toBe(false)
+  })
+  test('郵便番号に数字以外の値があるとき、isPostalCodeがfalseを返すこと', () => {
+    expect(isPostalCode('11a-000')).toBe(false)
+  })
+})
