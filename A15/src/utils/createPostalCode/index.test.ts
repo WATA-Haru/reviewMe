8244d9ba85b3002f484eb0ce220fcab355606876
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2024-12-27 16:45:17 +0900

    [FIX] 変数aをresultに変更

--- A15/src/utils/createPostalCode/index.test.ts
+++ A15/src/utils/createPostalCode/index.test.ts
@@ -4,12 +4,12 @@ import { createPostalCode } from './index.ts'
 
 describe('createPostalCode', () => {
   test('引数がPostalCodeのフォーマットに従っている場合(\"111-0000\")、返り値がPostalCode型になること', () => {
-    const a = createPostalCode('111-0000')
-    expect(isPostalCode(a)).toBe(true)
+    const result = createPostalCode('111-0000')
+    expect(isPostalCode(result)).toBe(true)
   })
 
   test('引数がPostalCodeのフォーマットに従っていない場合(\"2\")、返り値がnullになること', () => {
-    const a = createPostalCode('2')
-    expect(a).toBe(null)
+    const result = createPostalCode('2')
+    expect(result).toBe(null)
   })
 })
