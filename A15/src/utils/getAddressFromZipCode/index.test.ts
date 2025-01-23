c428cdbe7d0ec026db762c472d9aa2520ad9d1b8
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2025-01-23 17:41:02 +0900

    [FIX] createZipCodeをtest内で使わずTypeAssersionに変更

--- A15/src/utils/getAddressFromZipCode/index.test.ts
+++ A15/src/utils/getAddressFromZipCode/index.test.ts
@@ -2,6 +2,7 @@ import { describe, test, beforeAll, afterEach, afterAll, expect } from 'vitest'
 import { server } from '@/mocks/node'
 import { getAddressFromZipCode } from '@/utils/getAddressFromZipCode/index.ts'
 import { createZipCode } from '@/utils/createZipCode/index.ts'
+import { type ZipCode } from '@/types/ZipCode.ts'
 
 beforeAll(() => server.listen())
 afterEach(() => server.resetHandlers())
@@ -9,11 +10,9 @@ afterAll(() => server.close())
 
 describe('getAddressFromZipCode', () => {
   test('getAddressFromZipCodeが正常終了した際のstatusが200であること', async () => {
-    const zipCode = createZipCode('079-0177')
-    if (zipCode) {
-      const response = await getAddressFromZipCode(zipCode)
-      expect(response?.data?.status).toBe(200)
-    }
+    const zipCode = '079-0177' as ZipCode
+    const response = await getAddressFromZipCode(zipCode)
+    expect(response?.data?.status).toBe(200)
   })
   test('getAddressFromZipCodeに空の文字列を渡した際のstatusが400であること', async () => {
     const response = await getAddressFromZipCode(null)
