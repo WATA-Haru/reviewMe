96ae6cddbe1b99fcad7638a7bf7c101a02719d9e
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2025-01-25 15:40:16 +0900

    [FIX] mockのurlにクエリパラメータ(?zipcode)を入れていたので削除

--- A15/src/mocks/handlers.ts
+++ A15/src/mocks/handlers.ts
@@ -2,4 +2,4 @@ import { http } from 'msw'
 import { mocks } from '@/mocks/resolvers/mocks'
 import { zipCloudURLWrapper } from '@/utils/zipCloudURLWrapper/index.ts'
 
-export const handlers = [http.get(zipCloudURLWrapper('?zipcode'), mocks.get)]
+export const handlers = [http.get(zipCloudURLWrapper(), mocks.get)]
