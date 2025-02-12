0c03c0775cf6611a2117b7710b5c04c632b87df4
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2025-02-12 09:55:44 +0900

    [ADD] 全角数字を半角数字(文字列型)にして返す関数の作成
    String(number)で変換すると'０１２'が'12'になる問題があったので自作した

---
+++ A15/src/utils/makeFullWidthNumToHalfWidthNum/index.test.ts
@@ -0,0 +1,14 @@
+import { describe, test, expect } from 'vitest'
+import { makeFullWidthNumToHalfWidthNum } from './index.ts'
+
+describe('makeFullWidthNumToHalfWidthNum', () => {
+  test(`'１'を渡したとき、'1'が返ること`, () => {
+    expect(makeFullWidthNumToHalfWidthNum('０')).toBe('0')
+  })
+  test(`'０１２'を渡したとき、'01'が返ることと`, () => {
+    expect(makeFullWidthNumToHalfWidthNum('０１２')).toBe('012')
+  })
+  test(`'1a１'を渡したとき、'1a1'が返ることと`, () => {
+    expect(makeFullWidthNumToHalfWidthNum('１a1')).toBe('1a1')
+  })
+})
