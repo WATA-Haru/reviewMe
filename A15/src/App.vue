7bf9a65367e78b1be7e8e995f0c250a6261f09d2
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2025-02-12 10:56:13 +0900

    [ADD] App.vueにFormを追加

--- A15/src/App.vue
+++ A15/src/App.vue
@@ -1,5 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import FormContainer from '@/components/Form/container/index.vue'
+</script>
 
 <template>
-  <main></main>
+  <main>
+    <FormContainer />
+  </main>
 </template>
