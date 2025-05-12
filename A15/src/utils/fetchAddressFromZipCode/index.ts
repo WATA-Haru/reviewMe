7648566b9592111a862195ecd43caa5950796598
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2025-05-12 17:33:18 +0900

    [FIX] 無駄なコメント削除

--- A15/src/utils/fetchAddressFromZipCode/index.ts
+++ A15/src/utils/fetchAddressFromZipCode/index.ts
@@ -4,10 +4,6 @@ import { type AxiosResponse } from 'axios'
 import { type ZipCloudResponse } from '@/types/ZipCloudResponse.ts'
 import { type ZipCode } from '@/types/ZipCode.ts'
 
-//const instance = axios.create({
-//  baseURL: zipCloudURLWrapper(''),
-//})
-
 export const fetchAddressFromZipCode = async (
   zipCode: ZipCode | null,
 ): Promise<AxiosResponse<ZipCloudResponse, never> | void> => {
